a66d7b41036a90646244a4dcec55a99604969514
Andrew Olivas <andrewrolivas@gmail.com>
2014-11-06 08:34:13 -0800

    created a submit button

--- view/header.php
+++ view/header.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+<link href='http://fonts.googleapis.com/css?family=Cabin:700italic' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" href="css/phpmain.css">
 	<title> Blog - PHP </title>
 </head>
