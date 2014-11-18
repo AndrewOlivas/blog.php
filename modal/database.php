3d570b1b084e51eee446e8bb73ef261d38863bdd
Andrew Olivas <andrewrolivas@gmail.com>
2014-11-18 08:37:05 -0800

    milestones 30,and 31: closing connection and implemented the query function.

--- modal/database.php
+++ modal/database.php
@@ -30,12 +30,28 @@ class Database {
  	}
 	// closes connection to create-db.php
 	public function closeConnection() {
+		// to check whether or not youve opened a connection
+		// isset is to check if there is information or not
+		// if no info, it will make it null, so no need to close the connectio
+		// no need to call this variable if no info is there
+		if (isset ($this->connection)) {
+			$this->connection->close();
+		}
 
 	}
 	// makes the query public
 	// every time you call on our query function you have to pass it a string 
 	// dont have to repeat yourself with this code,before you had to repeat five lines of code.
 	public function query($string) {
+	// open database,query the database, get results, close database.
+		// this code is calling on a function
+		$this->connection();
+		// all info will be stored in $string
+		$query = $this->connection->query($string);
+
+		$this->closeConnection();
+
+		return $query;
 
 
 	}
