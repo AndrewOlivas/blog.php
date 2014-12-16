af29eb7078908a1f6e407bd1cd10b43572af0a9c
Andrew Olivas <andrewrolivas@gmail.com>
2014-12-16 07:54:34 -0800

    fixed the blog post not showing up after logging in as a user issue.

--- view/navigation.php
+++ view/navigation.php
@@ -3,38 +3,23 @@
 ?>
 
 <style type="text/css">
-		html { 
-     background: url(img/ironman-wp.jpg) 
-     no-repeat center center fixed; 
-     -webkit-background-size: cover;
-     -moz-background-size: cover;
-     -o-background-size: cover;
-     background-size: cover;
-}
+	
 
-Custom, iPhone Retina 
-@media (max-width : 375px) and (max-width: 482px) {
-	html,body {
-		background-color: grey;
-	}
-}
-
-Small Devices, Ipads 
-@media (min-width : 768px) and (max-device-width: 1080px) {
-	html,body{
-		background-color: aliceblue;
-	}
-
-	a:link {
-    color: #000000;
-}
-
-}
 </style>
 
 <nav>
 	<ul>
 		<!-- since $path = /_blog.php/, you can just put any file as seen below with post and it will direct you there -->
 		<li><a href="<?php echo $path . "post.php"?>">Blog Post Form</a></li>
+
+	</ul>
+</nav>
+
+<nav>
+	<ul>
+		
+		<li><a href="http://localhost/_blog.php/login.php">Login</a></li>
+		<li><a href="http://localhost/_blog.php/register.php">Register</a></li>
+
 	</ul>
 </nav>
